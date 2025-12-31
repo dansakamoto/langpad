2b4ebb51ed4c5831bd79cd2b188e1977ce96b941
Dan Sakamoto <contact@dansakamoto.com>
2025-12-31 15:30:35 -0500

    ci: test with API logging

--- src/server/routes/getAudio.ts
+++ src/server/routes/getAudio.ts
@@ -6,10 +6,13 @@ import type { Request, Response } from "express";
 
 const apiExceptions = ["十一", "二兆"];
 
+const fsLogging = true;
+
 export default async function getAudio(req: Request<string>, res: Response) {
   const serverPath = "downloads/ja/" + req.body.text + ".b64";
 
   if (fs.existsSync(serverPath)) {
+    if (fsLogging) console.log("File found, sending from local");
     fs.readFile(serverPath, "base64", (err, data) => {
       if (err) {
         console.error(err);
@@ -20,6 +23,8 @@ export default async function getAudio(req: Request<string>, res: Response) {
 
     return;
   }
+
+  if (fsLogging) console.log("File not found, requesting from API");
 
   const t = req.body.text;
   const num = !apiExceptions.includes(t) ? kanji2number(t) : t;
