2ab05114a5b8283cdd1e8614f3358f3cacc0fbe4
Dan Sakamoto <contact@dansakamoto.com>
2026-01-18 00:45:11 -0500

    feat: 404 redirect to index

--- src/server/main.ts
+++ src/server/main.ts
@@ -5,23 +5,27 @@ import getAudio from "./routes/getAudio";
 const port = 3000;
 const app = express();
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.json());
+
+app.post("/getAudio", getAudio);
+
 if (process.env.NODE_ENV == "production") {
   app.use("/", express.static("dist"));
 
   app.listen(port, () => {
     console.log(`Soundboard running in production mode on port ${port}`);
   });
+
+  app.use((_req, res) => {
+    res.redirect("/");
+  });
 } else {
   ViteExpress.listen(app, port, () => {
     console.log(`Soundboard running in development mode on port ${port}`);
   });
 }
-
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(express.json());
-
-app.post("/getAudio", getAudio);
